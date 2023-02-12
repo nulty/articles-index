a3446e9427aae40167dc9aa56c7746517d3689fc
Iain McNulty <iain@inulty.com>
2023-02-12 23:46:57 +0000

    Add ArticleMapping   ArticleMapping normalizes the json data that will   be imported into the Article model

---
+++ app/models/article_mapping.rb
@@ -0,0 +1,50 @@
+# Takes article_hash data from external source and
+# removes properties that are not desired
+# and rename specified properties
+class ArticleMapping
+  def initialize(article_hash)
+    @article_hash = article_hash
+  end
+
+  def call
+    filter
+    rename
+  end
+
+  RENAME = {
+    'id' => 'original_id',
+    'created_at' => 'original_created_at',
+    'updated_at' => 'original_updated_at'
+  }
+
+  PROPERTIES = %w[
+    id
+    title
+    description
+    location
+    value
+    section
+    status
+    expiry
+    reactions
+    photos
+    user
+    created_at
+    updated_at
+  ]
+
+  private
+
+  # rename the json properties that conflict with Rails model attributes
+  def rename
+    RENAME.each do |k, v|
+      @article_hash[v] = @article_hash.delete(k)
+    end
+    @article_hash
+  end
+
+  # select only the properties we expect from the article_hash
+  def filter
+    @article_hash.slice(*PROPERTIES)
+  end
+end
---
+++ test/models/article_mapping_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class ArticleMappingTest < ActiveSupport::TestCase
+  def setup
+    @user = {
+      'id' => 8039,
+      'first_name' => 'Lloyd',
+      'current_avatar' => {
+        'original' => 'https://cdn.olioex.com/uploads/avatar/file/oZq8DF3dzLEi3Fnf4XxMrg/image.jpg',
+        'large' => 'https://cdn.olioex.com/uploads/avatar/file/oZq8DF3dzLEi3Fnf4XxMrg/large_image.jpg',
+        'small' => 'https://cdn.olioex.com/uploads/avatar/file/oZq8DF3dzLEi3Fnf4XxMrg/small_image.jpg'
+      },
+      'roles' => ['Consumer', 'Food Waste Hero', 'Volunteer', 'Administrator'],
+      'location' => { 'latitude' => 51.7645, 'longitude' => -3.79133 },
+      'rating' => { 'rating' => 10, 'number' => 15 },
+      'verifications' => [{ 'method' => 'sms' }, { 'method' => 'facebook' }]
+    }
+  end
+
+  test 'article json is mapped to correct format' do
+    json = file_fixture('articles.json').read
+    article_data = JSON.parse(json).first
+    mapped = ArticleMapping.new(article_data).call
+    assert_equal mapped['user'], @user
+  end
+end
